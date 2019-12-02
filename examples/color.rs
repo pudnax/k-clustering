9150c35d721f0a7628b66300c9dfcec3c3af0cc0
Alex Komissarov <k.a.komissar@gmail.com>
2019-12-02 16:57:16 +0500

    Update example

--- examples/color.rs
+++ examples/color.rs
@@ -1,12 +1,15 @@
 use k_clustering::*;
 
 fn main() {
-    let img = image::open("in.jpg").unwrap();
-
     // let img = img.grayscale();
-    let img = img.blur(2.);
-
-    let mut kms = KMeansSegmentation::new(img, 5);
-    while (kms.iteration()) > 0 {}
-    kms.overlay_color().save("out.png").unwrap();
+    let name = "adel4.jpg";
+    for i in 0..50 {
+        let img = image::open(name).unwrap();
+        // let img = img.blur(2.);
+        let mut kms = KMeansSegmentation::new(img, i);
+        while (kms.iteration()) > 0 {}
+        kms.overlay_color()
+            .save(format!("adel3/adel_out{}.png", i))
+            .unwrap();
+    }
 }
